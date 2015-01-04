1051b0b19b57fee92444b4b10cc2b70877d4da06
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-01-04 13:45:55 +1100

    Change to base layout on qwerty--ben

--- firmware/keyboard/ergodox/layout/custom-layout.c
+++ firmware/keyboard/ergodox/layout/custom-layout.c
@@ -1,54 +1,145 @@
 /* ----------------------------------------------------------------------------
- * Copyright (c) 2013 Ben Blazak <benblazak.dev@gmail.com>
+ * Copyright (c) 2013, 2014 Ben Blazak <benblazak.dev@gmail.com>
  * Released under The MIT License (see "doc/licenses/MIT.md")
  * Project located at <https://github.com/benblazak/ergodox-firmware>
  * ------------------------------------------------------------------------- */
 
 /**                                                                 description
- * A QWERTY layout using the "kinesis-mod" template
+ * My QWERTY layout, at the moment.  I imagine this will evolve over time.
+ * Once I'm done with the Arensito layout, it may disappear altogether.
  *
  * Implements the "layout" section of '.../firmware/keyboard.h'
  */
 
 
-// left hand letter key block
-#define  T_q        q
-#define  T_w        w
-#define  T_e        e
-#define  T_r        r
-#define  T_t        t
-#define  T_a        a
-#define  T_s        s
-#define  T_d        d
-#define  T_f        f
-#define  T_g        g
-#define  T_z        z
-#define  T_x        x
-#define  T_c        c
-#define  T_v        v
-#define  T_b        b
-
-// right hand letter key block
-#define  T_y        y
-#define  T_u        u
-#define  T_i        i
-#define  T_o        o
-#define  T_p        p
-#define  T_h        h
-#define  T_j        j
-#define  T_k        k
-#define  T_l        l
-#define  T_semicol  semicol
-#define  T_n        n
-#define  T_m        m
-#define  T_comma    comma
-#define  T_period   period
-#define  T_slash    slash
-
-// right hand outer key block
-#define  T_bkslash  bkslash
-#define  T_quote    quote
+#include "./fragments/includes.part.h"
+#include "./fragments/macros.part.h"
+#include "./fragments/types.part.h"
+#include "./fragments/variables.part.h"
 
 
-#include "./templates/kinesis-mod.c.h"
+// ----------------------------------------------------------------------------
+// keys
+// ----------------------------------------------------------------------------
+
+#include "./fragments/keys.part.h"
+
+
+// ----------------------------------------------------------------------------
+// LED control
+// ----------------------------------------------------------------------------
+
+#include "./fragments/led-control.part.h"
+
+
+// ----------------------------------------------------------------------------
+// matrix control
+// ----------------------------------------------------------------------------
+
+#include "./fragments/matrix-control.part.h"
+
+
+// ----------------------------------------------------------------------------
+// layout
+// ----------------------------------------------------------------------------
+
+static layout_t layout PROGMEM = {
+// ............................................................................
+
+    MATRIX_LAYER(  // layer 0 : default
+// macro, unused,
+       K,    nop,
+// left hand ...... ......... ......... ......... ......... ......... .........
+   equal,        1,        2,        3,        4,        5,      esc,
+     tab,        q,        w,        e,        r,        t,   lpu2l2,
+ bkslash,        a,        s,        d,        f,        g,
+shL2kcap,        z,        x,        c,        v,        b, lpupo2l2,
+    guiL,    grave,  bkslash,   arrowL,   arrowR,
+                                                               ctrlL,     altL,
+                                                       nop,      nop,     home,
+                                                        bs,      del,      end,
+// right hand ..... ......... ......... ......... ......... ......... .........
+            lpu1l1,        6,        7,        8,        9,        0,     dash,
+             brktL,        y,        u,        i,        o,        p,    brktR,
+                           h,        j,        k,        l,  semicol,    quote,
+          lpupo2l2,        n,        m,    comma,   period,    slash, shR2kcap,
+                                arrowL,   arrowD,   arrowU,   arrowR,     guiR,
+    altR,    ctrlR,
+   pageU,      nop,      nop,
+   pageD,    enter,    space  ),
+
+// ............................................................................
+
+    MATRIX_LAYER(  // layer 1 : number pad
+// macro, unused,
+       K,    nop,
+// left hand ...... ......... ......... ......... ......... ......... .........
+  transp,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,   transp,   transp,   transp,   transp,   transp,
+  transp,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,      ins,   transp,   transp,   transp,
+                                                              transp,   transp,
+                                                    transp,   transp,   transp,
+                                                    transp,   transp,   transp,
+// right hand ..... ......... ......... ......... ......... ......... .........
+            lpo1l1,   transp,   lpo1l1,    equal,    slash, asterisk,   transp,
+            transp,   transp,        7,        8,        9,     dash,   transp,
+                      transp,        4,        5,        6,     plus,   transp,
+            transp,   transp,        1,        2,        3,    enter,   transp,
+                                transp,   transp,   period,    enter,   transp,
+  transp,   transp,
+  transp,   transp,   transp,
+  transp,   transp,        0  ),
+
+// ............................................................................
+
+    MATRIX_LAYER(  // layer 2 : symbols and function keys
+// macro, unused,
+       K,    nop,
+// left hand ...... ......... ......... ......... ......... ......... .........
+  transp,       F1,       F2,       F3,       F4,       F5,      F11,
+  transp,   braceL,   braceR,    brktL,    brktR,      nop,   lpo2l2,
+  transp,  semicol,    slash,     dash,        0,    colon,
+  transp,        6,        7,        8,        9,     plus, lpupo3l3,
+  transp,   transp,   transp,   transp,   transp,
+                                                              transp,   transp,
+                                                    transp,   transp,   transp,
+                                                    transp,   transp,   transp,
+// right hand ..... ......... ......... ......... ......... ......... .........
+               F12,       F6,       F7,       F8,       F9,      F10,    power,
+            lpo2l2,    caret,  undersc, lessThan, grtrThan,   dollar,  volumeU,
+                     bkslash,        1,   parenL,   parenR,    equal,  volumeD,
+          lpupo3l3, asterisk,        2,        3,        4,        5,     mute,
+                                transp,   transp,   transp,   transp,   transp,
+  transp,   transp,
+  transp,   transp,   transp,
+  transp,   transp,   transp  ),
+
+// ............................................................................
+
+    MATRIX_LAYER(  // layer 3 : keyboard functions
+// macro, unused,
+       K,    nop,
+// left hand ...... ......... ......... ......... ......... ......... .........
+   btldr,      nop,      nop,      nop,      nop,      nop,      nop,
+     nop,      nop,      nop,      nop,      nop,      nop,      nop,
+     nop,      nop,      nop,      nop,      nop,      nop,
+     nop,      nop,      nop,      nop,      nop,      nop,      nop,
+     nop,      nop,      nop,      nop,      nop,
+                                                                 nop,      nop,
+                                                       nop,      nop,      nop,
+                                                       nop,      nop,      nop,
+// right hand ..... ......... ......... ......... ......... ......... .........
+               nop,      nop,      nop,      nop,      nop,      nop, dmp_sram,
+               nop,      nop,      nop,      nop,      nop,      nop, dmp_prog,
+                         nop,      nop,      nop,      nop,      nop, dmp_eepr,
+               nop,      nop,      nop,      nop,      nop,      nop,      nop,
+                                   nop,      nop,      nop,      nop,      nop,
+     nop,      nop,
+     nop,      nop,      nop,
+     nop,      nop,      nop  ),
+
+// ............................................................................
+};
 
